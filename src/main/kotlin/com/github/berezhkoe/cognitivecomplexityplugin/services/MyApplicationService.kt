147bba04f591d75b60418dfc18ab9cf3b55b2fe9
GitHub Action <action@github.com>
2021-10-10 10:33:36 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/berezhkoe/cognitivecomplexityplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.berezhkoe.cognitivecomplexityplugin.services
+
+import com.github.berezhkoe.cognitivecomplexityplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
