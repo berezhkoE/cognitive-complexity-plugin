7c6be9883c3a97df368f2a231a68d7dd099da679
berezhkoE <berezhko.ei@gmail.com>
2021-12-28 19:17:06 +0300

    State annotation for kotlin settings added

--- src/main/kotlin/cc/kotlin/settings/KtCCSettings.kt
+++ src/main/kotlin/cc/kotlin/settings/KtCCSettings.kt
@@ -1,8 +1,11 @@
 package cc.kotlin.settings
 
 import com.intellij.openapi.components.SimplePersistentStateComponent
+import com.intellij.openapi.components.State
+import com.intellij.openapi.components.Storage
 import com.intellij.openapi.components.service
 
+@State(name = "KotlinCCSetting", storages = [(Storage("cognitive_complexity_kotlin.xml"))])
 class KtCCSettings :
     SimplePersistentStateComponent<KtCCSettingsState>(KtCCSettingsState()) {
 
