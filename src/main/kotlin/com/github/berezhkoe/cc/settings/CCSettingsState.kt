8141dcc8f34b9aa45e84b356f0f09f5f66624922
berezhkoE <berezhko.ei@gmail.com>
2022-02-06 00:10:14 +0300

    Default thresholds added

--- src/main/kotlin/com/github/berezhkoe/cc/settings/CCSettingsState.kt
+++ src/main/kotlin/com/github/berezhkoe/cc/settings/CCSettingsState.kt
@@ -2,15 +2,16 @@ package com.github.berezhkoe.cc.settings
 
 import com.github.berezhkoe.cc.CCBundle.message
 import com.intellij.openapi.components.BaseState
+import com.intellij.util.containers.addAllIfNotNull
 
-class CCSettingsState: BaseState() {
+class CCSettingsState : BaseState() {
     var thresholdsList by list<ThresholdState>()
 
     var defaultText by string(message("default.hint.text"))
 
     var showBeforeAnnotations by property(true)
 
-    class ThresholdState: BaseState() {
+    class ThresholdState : BaseState() {
         companion object {
             fun fromMapping(threshold: Int, color: String, text: String) = ThresholdState().apply {
                 this.threshold = threshold
@@ -24,4 +25,12 @@ class CCSettingsState: BaseState() {
         var text by string("")
     }
 
+    init {
+        thresholdsList.addAllIfNotNull(
+            ThresholdState.fromMapping(0, "Gray", message("default.hint.text")),
+            ThresholdState.fromMapping(10, "Blue", message("default.hint.text")),
+            ThresholdState.fromMapping(50, "Green", message("default.hint.text")),
+            ThresholdState.fromMapping(80, "Violet", message("default.hint.text")),
+        )
+    }
 }
